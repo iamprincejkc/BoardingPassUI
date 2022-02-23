1cec2922f2b495dd233b229a5615fab5f1b60fb2
Jan Kevin Cadampog <jkcadampog.rdpi@gmail.com>
2022-02-23 13:46:44 +0800

    Add project files.

---
+++ BoardingPassUI/BoardingPassUI/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("cabinsketch.ttf", Alias = "CabinSketch")]
+[assembly: ExportFont("materialdesigniconswebfont.ttf", Alias = "FontAwesome")]
+[assembly: ExportFont("robotoregular.ttf", Alias = "Roboto")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
---
+++ BoardingPassUI/BoardingPassUI/BoardingPass.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BoardingPassUI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoardingPass : ContentPage
+    {
+        public BoardingPass()
+        {
+            InitializeComponent();
+        }
+    }
+}
